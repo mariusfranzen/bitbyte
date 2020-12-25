065b2c5fc3e47ea6ecef8af45db80989729968ad
Marius Franzén <marius.franzen1@gmail.com>
2020-12-25 20:43:24 +0100

    Added basic front- and back-end

---
+++ blog/src/main/java/com/bitbyte/blog/BitbyteBlogApplication.java
@@ -0,0 +1,13 @@
+package com.bitbyte.blog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BitbyteBlogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BitbyteBlogApplication.class, args);
+	}
+
+}
